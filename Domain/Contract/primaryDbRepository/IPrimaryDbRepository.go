0f07b15792dd13349cd5d9d5f8fe61ee05f641da
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-04 08:03:32 +0700

    feat<domain>: define `IPrimaryDbRepository` function

--- Domain/Contract/primaryDbRepository/IPrimaryDbRepository.go
+++ Domain/Contract/primaryDbRepository/IPrimaryDbRepository.go
@@ -3,4 +3,8 @@ package primaryDbRepository
 import "github.com/GoDriveApp/GoDriveApi/Core/Entity"
 
 type IPrimaryDbRepository[T Entity.BaseEntity] interface {
+	Save(entity T)
+	Update(entity T)
+	Delete(entity T)
+	GetById(id string)
 }
