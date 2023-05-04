6aa56a92c7b445cef9d6d7887a4d468e540a572a
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-04 08:04:12 +0700

    feat<infra>: implement `IPrimaryDbRepository` functions, but not finished

---
+++ Infra/PrimaryDbRepository/PrimaryDbRepository.go
@@ -0,0 +1,28 @@
+package PrimaryDbRepository
+
+import "github.com/GoDriveApp/GoDriveApi/Core/Entity"
+
+// TODO implement all functions
+
+type PrimaryDbRepository[T Entity.BaseEntity] struct {
+}
+
+func NewPrimaryDbRepository[T Entity.BaseEntity]() PrimaryDbRepository[T] {
+	return PrimaryDbRepository[T]{}
+}
+
+func (repo *PrimaryDbRepository[T]) Save(entity T) {
+
+}
+
+func (repo *PrimaryDbRepository[T]) Update(entity T) {
+
+}
+
+func (repo *PrimaryDbRepository[T]) Delete(entity T) {
+
+}
+
+func (repo *PrimaryDbRepository[T]) GetById(id string) {
+
+}
